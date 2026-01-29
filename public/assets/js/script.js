cc1bb313907f4b34c226db9f1fc1c697260b3b82
avxsh9 <125174963+avxsh9@users.noreply.github.com>
2026-01-30 00:33:28 +0530

    Initial commit

---
+++ public/assets/js/script.js
@@ -0,0 +1,11 @@
+console.log('TickerAdda Frontend Loaded');
+
+// Sticky Navbar Effect
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
